ed1aeed4126a53413ff4cd5e08394d815147c504
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-12-26 22:14:46 -0500

    Switch time to 24h

--- src/components/time.js
+++ src/components/time.js
@@ -4,11 +4,7 @@ import { ChronoField } from "@js-joda/core"
 
 export default ({ value }) => {
   const meridiem = value.get(ChronoField.AMPM_OF_DAY) === 0 ? 'am' : 'pm'
-  let hour = value.get(ChronoField.HOUR_OF_AMPM)
-  if (hour === 0) {
-    hour = 12
-  }
+  let hour = value.get(ChronoField.HOUR_OF_DAY)
   const minute = value.get(ChronoField.MINUTE_OF_HOUR)
-
-  return (<span>{hour}:{minute.toString().padStart(2, '0')}{meridiem === 'am' ? 'a' : 'p'}</span>)
+  return (<span>{hour.toString().padStart(2, '0')}:{minute.toString().padStart(2, '0')}</span>)
 }
