d681fdf2622deb14606cc51f44f9fd6ed6bb2050
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-12-27 00:57:26 -0500

    inline two modules

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,14 +1,79 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
 
-import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+
+import { Link, graphql } from "gatsby"
 
 import Layout from "@/components/layout.js"
+import Time from "@/components/time.js"
 
-import Now from "@/client/now.js"
+import Show, { sortByStart, zeitgeist } from "@/models/show.js"
 
-import DailySchedule from "@/client/daily-schedule.js"
+const Now = ({ now, next }) => {
+  return (
+    <>
+      <p><Link to={now.show.slug}>{now.show.title}</Link> is currently on air.</p>
+      {next ? (<p>Up next is <Link to={next.show.slug}>{next.show.title}</Link>.</p>) : null}
+    </>
+  )
+}
 
-import Show, { zeitgeist } from "@/models/show.js"
+const Day = ({ day, airshifts }) => {
+  return (
+    <Fragment key={day}>
+      <table>
+        <tbody>
+          {airshifts.map(([show, airshift]) => {
+            return (
+              <tr key={show.id}>
+                <td><Time value={airshift.start} /> - <Time value={airshift.end} /></td>
+                <td><Link to={show.slug}>{show.title}</Link></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </Fragment>
+  )
+}
+
+const DailySchedule = ({ shows }) => {
+  const allAirshifts = shows.map((show) => {
+    return show.airshifts.map((airshift) => [show, airshift])
+  }).reduce((accumulation, item) => accumulation.concat(item), [])
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  const [ day, setDay ] = useState(days[0])
+  const airshifts = allAirshifts.filter(([show, airshift]) => airshift.day === day).sort((x, y) => sortByStart(x[1], y[1]))
+
+  return (
+    <>
+      <p>
+        {days.map((givenDay) => {
+          return (
+            <button
+              onClick={(e) => setDay(givenDay)}
+              css={css`
+                border: none;
+                color: ${day === givenDay ? 'black' : 'blue'};
+                text-decoration: ${day === givenDay ? 'none' : 'underline'};
+                background: transparent;
+                height: 1.6em;
+                text-align: center;
+                display: inline-block;
+              `}
+              key={givenDay}
+            >
+              {givenDay}
+            </button>
+          )
+        })}
+      </p>
+      <Day day={day} airshifts={airshifts} />
+    </>
+  )
+}
 
 export default ({ data }) => {
   const shows = data.allMarkdownRemark.edges.map((edge) => edge.node).map(Show.factory)
