8e8d0670c352b3d41de30b3bfb39d0207c0ba07e
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-03-12 19:17:07 -0400

    Replace body-based pages with frontmatter-based drafting

--- src/templates/pages/single.js
+++ src/templates/pages/single.js
@@ -3,12 +3,27 @@ import { graphql } from "gatsby"
 
 import Layout from "@/components/layout.js"
 
+import { css } from "@emotion/core"
+
+import remark from "remark"
+import html from "remark-html"
+
 export default ({ data }) => {
   const page = data.markdownRemark
+  const sections = page.frontmatter.sections
 
   return (
     <Layout>
-      <div dangerouslySetInnerHTML={{ __html: page.html }} />
+      {sections && sections.map((section, index) => {
+        switch (section.template) {
+          case "image":
+            return <img key={index} src={section.image} css={css`width: 100%; margin: 1em 0`} />
+          case "text":
+            return <div key={index} dangerouslySetInnerHTML={{ __html: remark().use(html).processSync(section.content).toString() }}></div>
+          default:
+            throw new Error(`Unknown section template \`${section.template}\``)
+        }
+      })}
     </Layout>
   )
 }
@@ -17,6 +32,13 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      frontmatter {
+        sections {
+          template
+          image
+          content
+        }
+      }
     }
   }
 `
