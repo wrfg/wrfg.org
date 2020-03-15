8dbb60f4e17708b5dcc1c7d38e9872598398c5f9
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-03-15 15:05:18 -0400

    Inline head.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 
 import { Global, css } from "@emotion/core"
 
-import Head from './head.js'
+import { Helmet } from "react-helmet-async"
 
 import { ReadableContainer, Section, Clear, Left, Right } from "./parts.js"
 
@@ -64,7 +64,18 @@ export default ({ children }) => {
 
   return (
     <>
-      <Head />
+      <Helmet
+        title={`WRFG`}
+        meta={[
+          {name: 'charset', content: 'utf-8'},
+          {name: 'apple-mobile-web-app-title', content: 'WRFG'},
+          //{name: 'apple-mobile-web-app-capable', content: 'yes'},// until navigation can be done without changing the url, setting this is a subpar experience
+        ]}
+        link={[
+          {rel: 'apple-touch-startup-image', href: '/images/logo-disk-white.png'},
+          {rel: 'apple-touch-icon', href: '/images/logo-disk-white.png'},
+        ]}
+      />
       <Global
         styles={css`
           body {
