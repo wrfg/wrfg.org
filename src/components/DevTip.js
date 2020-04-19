a28339d97f5f4800224e5ff8062f5d80628728a9
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-04-19 15:57:33 -0400

    Adjust message

--- src/components/DevTip.js
+++ src/components/DevTip.js
@@ -8,7 +8,7 @@ const DevTip = ({ visible, children }) => {
   }
 
   return <div css={css`padding: 1em; border: 1px solid pink`}>
-    <div><small>This is only displayed in testing mode. Testing mode has been enabled for demonstration purposes on unofficial.wrfg.org.</small></div>
+    <div><small>This message is only displayed in testing mode. Testing mode has been enabled for demonstration purposes on unofficial.wrfg.org.</small></div>
     {children}
   </div>
 }
